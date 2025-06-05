1b10db03b6b97e7b94a9bedf2f07feba1b03eab2
Arduncandev <103537497+Arduncandev@users.noreply.github.com>
2025-06-05 08:53:32 -0500

    inital commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+}
