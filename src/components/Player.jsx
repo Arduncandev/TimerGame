94486fe206acd3c43a18151d3aab099ff0687a39
Arduncandev <103537497+Arduncandev@users.noreply.github.com>
2025-06-05 09:10:53 -0500

    changed Playername to use Ref instead of two state values

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState('');
+	const playerName = useRef();
 
-  function handleChange(event) {
-    setPlayerName(event.target.value);
-  }
+	const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+
+	function handleClick() {
+		setEnteredPlayerName(playerName.current.value);
+	}
+
+	return (
+		<section id='player'>
+			<h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+			<p>
+				<input
+					ref={playerName}
+					type='text'
+				/>
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
